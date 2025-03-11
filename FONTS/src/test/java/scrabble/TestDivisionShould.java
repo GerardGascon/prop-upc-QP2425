7d660206ffeebedebc254a1c72ad153bdf040999
Gerard Gascón <ggasconmoline@gmail.com>
2025-03-11 16:25:41 +0100

    init

---
+++ FONTS/src/test/java/scrabble/TestDivisionShould.java
@@ -0,0 +1,24 @@
+package scrabble;
+
+import static org.junit.Assert.assertEquals;
+
+import edu.upc.prop.scrabble.Main;
+import org.junit.Test;
+
+public class TestDivisionShould {
+  @Test
+  public void divideGettingCorrectResult() {
+    Main divisionMachine = new Main();
+
+    float actualResult = divisionMachine.division(10,2);
+    assertEquals(5.0, actualResult, 0.0001);
+  }
+
+  @Test(expected = ArithmeticException.class)
+  public void divideByZeroThrowsException() {
+    Main divisionMachine = new Main();
+
+    float actualResult = divisionMachine.division(10,0);
+
+  }
+}
