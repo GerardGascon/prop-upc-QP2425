1cc5ed0a3f2d6a8addbd9517cda130f7dd0491f0
Gerard Gascón <ggasconmoline@gmail.com>
2025-03-11 17:33:27 +0100

    feat: created board class

---
+++ FONTS/main/edu/upc/prop/scrabble/data/Board.java
@@ -0,0 +1,13 @@
+package edu.upc.prop.scrabble.data;
+
+public class Board {
+    private final int size;
+
+    public Board(int size) {
+        this.size = size;
+    }
+
+    public int getSize() {
+        return size;
+    }
+}
