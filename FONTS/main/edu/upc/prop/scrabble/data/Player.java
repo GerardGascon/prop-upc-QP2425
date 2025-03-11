587f1b24a4cf6cec28944feaae7988162e0ba5e2
Alusat <alusat22@gmail.com>
2025-03-11 20:06:14 +0100

    Creació del Player. Evidentment encara falten atributs.

---
+++ FONTS/main/edu/upc/prop/scrabble/data/Player.java
@@ -0,0 +1,11 @@
+package edu.upc.prop.scrabble.data;
+
+public class Player {
+    private String name;
+
+    public Player(String name) {this.name = name;}
+
+    public String getName() {return name;}
+
+    private void setName(String name) {this.name = name;}
+}
