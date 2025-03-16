39b3adc434673b67de963e3ad99f2ca79c2ad3ab
Alusat <alusat22@gmail.com>
2025-03-16 17:10:58 +0100

    Algunos comentarios que hablar el martes + PlacePiece. Demomento no hay subclases Persona/CPU como tal.

--- FONTS/main/edu/upc/prop/scrabble/data/Player.java
+++ FONTS/main/edu/upc/prop/scrabble/data/Player.java
@@ -1,5 +1,6 @@
 package edu.upc.prop.scrabble.data;
 
+import java.util.Scanner;
 import java.util.Vector;
 
 public class Player {
@@ -14,15 +15,33 @@ public class Player {
 
     private void setName(String name) {this.name = name;}
 
-    private boolean CPU = false;
+    private boolean CPU = false; //fer la subclasse després o no cal?
+    // Demomento hacen lo mismo
 
     public boolean getCPU() {return CPU;}
 
     private void setCPU(boolean CPU) {this.CPU = CPU;}
 
-    private char[] hand = new char[7];//7 letras como mucho según las normas
+    private String[] hand = new String[7];//7 letras como mucho según las normas
 
-    public char[] getHand() {return hand;}
+    public String[] getHand() {return hand;} //cambiar de String(o char) a pieces
+    //o quizas no hacen falta que haya algo que sea piece: String valor y miramos el valor luego
 
-    public void setHand(char[] hand) {this.hand = hand;}
+   // public void setHand(String[] hand) {this.hand = hand;} no hace falta, robar
+
+    /*public void drawPiece(Bag bag){
+        if(hand.lenght < 7) hand.add(bag.getPiece());
+    }
+     */
+    public void placePiece(Board board) {
+        Scanner in  = new Scanner(System.in);
+        int x  = in.nextInt();
+        int y = in.nextInt();
+        int numeropieza = in.nextInt();
+        //hacer ifs o chequear de alguna manera que el numero de la pieza
+        //esté en la mano (o no se pase)
+        //que x y esten dentro lo mira board
+        board.placePiece(hand[numeropieza], x, y);
+        //cambié hand de chars a strings pq asi esta en board
+    }
 }
