9ff568e1bc69e0db1b09c7c3d17f4370cf285e4b
Gerard Gascón <ggasconmoline@gmail.com>
2025-03-16 15:43:17 +0100

    test: check if empty cells are marked as null

--- FONTS/test/scrabble/TestBoard.java
+++ FONTS/test/scrabble/TestBoard.java
@@ -4,6 +4,7 @@ import edu.upc.prop.scrabble.data.Board;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
 
 public class TestBoard {
     @Test
@@ -29,5 +30,12 @@ public class TestBoard {
         sut.placePiece("D", 0, 0);
 
         assertEquals("D", sut.getCell(0, 0));
+    }
+
+    @Test
+    public void readEmptyCellFromBoardReturnsNull() {
+        Board sut = new Board(4);
+
+        assertNull(sut.getCell(1, 1));
     }
 }
