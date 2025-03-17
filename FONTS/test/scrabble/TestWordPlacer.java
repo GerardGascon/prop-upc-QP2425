636b471726eecdcb757d5a3bf049af090a7ece7f
Gerard Gascón <52170489+GerardGascon@users.noreply.github.com>
2025-03-17 22:58:00 +0100

    test: separate board and word placer tests

---
+++ FONTS/test/scrabble/TestWordPlacer.java
@@ -0,0 +1,59 @@
+package scrabble;
+
+import edu.upc.prop.scrabble.data.board.Board;
+import edu.upc.prop.scrabble.data.board.StandardBoard;
+import edu.upc.prop.scrabble.domain.WordPlacer;
+import edu.upc.prop.scrabble.utils.Direction;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class TestWordPlacer {
+    @Test
+    public void placeWordVerticalPlacesWord() {
+        Board board = new StandardBoard();
+        BoardViewMock mock = new BoardViewMock();
+        WordPlacer sut = new WordPlacer(board, mock);
+
+        sut.run("TEST", 0, 0, Direction.Vertical);
+
+        assertEquals("T", board.getCellPiece(0, 0));
+        assertEquals("E", board.getCellPiece(0, 1));
+        assertEquals("S", board.getCellPiece(0, 2));
+        assertEquals("T", board.getCellPiece(0, 3));
+    }
+
+    @Test
+    public void placeWordHorizontalPlacesWord() {
+        Board board = new StandardBoard();
+        BoardViewMock mock = new BoardViewMock();
+        WordPlacer sut = new WordPlacer(board, mock);
+
+        sut.run("TEST", 0, 0, Direction.Horizontal);
+
+        assertEquals("T", board.getCellPiece(0, 0));
+        assertEquals("E", board.getCellPiece(1, 0));
+        assertEquals("S", board.getCellPiece(2, 0));
+        assertEquals("T", board.getCellPiece(3, 0));
+    }
+
+    @Test
+    public void noPlaceActionDoesntUpdateBoard(){
+        Board board = new StandardBoard();
+        BoardViewMock mock = new BoardViewMock();
+        WordPlacer _ = new WordPlacer(board, mock);
+
+        assertFalse(mock.getUpdateCallReceived());
+    }
+
+    @Test
+    public void placeActionUpdatesBoard(){
+        Board board = new StandardBoard();
+        BoardViewMock mock = new BoardViewMock();
+        WordPlacer sut = new WordPlacer(board, mock);
+
+        sut.run("TEST", 0, 0, Direction.Horizontal);
+
+        assertTrue(mock.getUpdateCallReceived());
+    }
+}
