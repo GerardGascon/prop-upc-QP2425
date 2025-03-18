8884153c6af6721dbbaca99ca1883bc27a6e22e9
Gerard Gascón <ggasconmoline@gmail.com>
2025-03-18 17:52:40 +0100

    feat: started parsing movement position

--- FONTS/main/edu/upc/prop/scrabble/domain/MovementReader.java
+++ FONTS/main/edu/upc/prop/scrabble/domain/MovementReader.java
@@ -2,6 +2,10 @@ package edu.upc.prop.scrabble.domain;
 
 import edu.upc.prop.scrabble.data.Movement;
 import edu.upc.prop.scrabble.utils.Direction;
+import edu.upc.prop.scrabble.utils.Pair;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class MovementReader {
     private final IMovementReader reader;
@@ -13,6 +17,31 @@ public class MovementReader {
     public Movement run() {
         String movementRaw = reader.readMove();
 
-        return new Movement("a", 3, 2, Direction.Horizontal);
+        String movementWord = parseMovementWord(movementRaw);
+        Pair<Integer, Integer> position = parseMovementPosition(movementRaw);
+
+        return new Movement(movementWord, 3, 2, Direction.Horizontal);
+    }
+
+    private Pair<Integer, Integer> parseMovementPosition(String movement) {
+        String direction = movement.split("\\s+")[1];
+
+        Pattern letterPattern = Pattern.compile("[A-Za-z]+");
+        Matcher letterMatcher = letterPattern.matcher(direction);
+
+        Pattern numberPattern = Pattern.compile("\\d+");
+        Matcher numberMatcher = numberPattern.matcher(direction);
+
+        String letter = letterMatcher.group();
+        String number = numberMatcher.group();
+
+        int x = letter.charAt(0) - (int)'A';
+        int y = Integer.parseInt(number);
+
+        return new Pair<>(x, y);
+    }
+
+    private String parseMovementWord(String movement) {
+        return movement.split("\\s+")[0];
     }
 }
--- FONTS/test/scrabble/TestMovementReader.java
+++ FONTS/test/scrabble/TestMovementReader.java
@@ -9,12 +9,33 @@ import static org.junit.Assert.*;
 
 public class TestMovementReader {
     @Test
-    public void testReadMoveReturnsCorrectMovement() {
+    public void testReadMoveReturnsCorrectWord() {
         IMovementReader reader = new MovementReaderMock("TEST C0");
         MovementReader sut = new MovementReader(reader);
 
         Movement res = sut.run();
 
         assertEquals("TEST", res.word());
+    }
+
+    @Test
+    public void testReadMoveReturnsCorrectPosition() {
+        IMovementReader reader = new MovementReaderMock("TEST C0");
+        MovementReader sut = new MovementReader(reader);
+
+        Movement res = sut.run();
+
+        assertEquals("TEST", res.word());
+    }
+
+    @Test
+    public void testReadMoveReturnsCorrectPositionInverted() {
+        IMovementReader reader = new MovementReaderMock("TEST 0C");
+        MovementReader sut = new MovementReader(reader);
+
+        Movement res = sut.run();
+
+        assertEquals(2, res.x());
+        assertEquals(0, res.y());
     }
 }
