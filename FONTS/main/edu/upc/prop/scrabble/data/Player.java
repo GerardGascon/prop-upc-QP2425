14eb5bca35fa55661dd0ab4a9846f3d1f0459b5c
Alusat <alusat22@gmail.com>
2025-03-18 15:22:54 +0100

    Puesta en comun martes

--- FONTS/main/edu/upc/prop/scrabble/data/Player.java
+++ FONTS/main/edu/upc/prop/scrabble/data/Player.java
@@ -7,45 +7,33 @@ import java.util.Vector;
 
 public class Player {
     private String name;
-
-    public Player(String name, boolean CPU) { //añadir password?
+    private Integer score;
+    public Player(String name, boolean CPU) {
         this.name = name;
         this.CPU = CPU;
+        this.score = 0;
     }
 
     public String getName() {return name;}
 
     private void setName(String name) {this.name = name;}
 
-    private boolean CPU = false; //fer la subclasse després o no cal?
-    // Demomento hacen lo mismo
+    private boolean CPU = false;//cambiar luego quizas
 
     public boolean getCPU() {return CPU;}
 
     private void setCPU(boolean CPU) {this.CPU = CPU;}
 
-    private Vector<Piece> hand = new Vector <Piece>(7);  //7 letras como mucho según las normas
+    public Integer getScore() {return score;}
 
-    public Vector<Piece> getHand() {return hand;} //cambiar de String(o char) a pieces
-    //o quizas no hacen falta que haya algo que sea piece: String valor y miramos el valor luego
+    private void setScore(Integer score) {this.score = score;}
 
-    // public void setHand(String[] hand) {this.hand = hand;} no hace falta, robar
+    private Vector<Piece> hand = new Vector <Piece>(7);
+
+    public Vector<Piece> getHand() {return hand;}
 
     public void drawPiece(Bag bag){
         if(hand.size() < 7) hand.add(bag.drawPiece());
-    }
-
-    public void placePiece(Board board) {
-        Scanner in  = new Scanner(System.in);
-        int x  = in.nextInt();
-        int y = in.nextInt();
-        int numeropieza = in.nextInt();
-        //hacer ifs o chequear de alguna manera que el numero de la pieza
-        //esté en la mano (o no se pase)
-        //que x y esten dentro lo mira board
-
-        //board.placePiece(hand.get(numeropieza), x, y); place pice pide un string
-
-        //cambié hand de chars a strings pq asi esta en board
+        //posar throw si
     }
 }
