bd2deb44dde6dd4b4e775f18faf353844e0487d6
Gerard Gascón <ggasconmoline@gmail.com>
2025-03-18 11:20:42 +0100

    feat: piece generator generates an array of pieces

--- FONTS/main/edu/upc/prop/scrabble/domain/PieceGenerator.java
+++ FONTS/main/edu/upc/prop/scrabble/domain/PieceGenerator.java
@@ -3,7 +3,18 @@ package edu.upc.prop.scrabble.domain;
 import edu.upc.prop.scrabble.data.Piece;
 
 public class PieceGenerator {
-    public Piece generatePiece(String piece) {
+    public Piece[] run(String pieces) {
+        String[] pieceArray = pieces.lines().toArray(String[]::new);
+        Piece[] result = new Piece[pieceArray.length];
+
+        for (int i = 0; i < pieceArray.length; i++) {
+            result[i] = generatePiece(pieceArray[i]);
+        }
+
+        return result;
+    }
+
+    private Piece generatePiece(String piece) {
         char c = parseCharacter(piece);
         int value = parseValue(piece);
 
@@ -11,7 +22,8 @@ public class PieceGenerator {
     }
 
     private int parseValue(String piece) {
-        return 1;
+        String[] values = piece.split("\\s+");
+        return Integer.parseInt(values[2]);
     }
 
     private char parseCharacter(String piece) {
--- FONTS/test/scrabble/TestPieceGenerator.java
+++ FONTS/test/scrabble/TestPieceGenerator.java
@@ -12,9 +12,9 @@ public class TestPieceGenerator {
         String dPiece = "D 4 2";
         PieceGenerator sut = new PieceGenerator();
 
-        Piece p = sut.generatePiece(dPiece);
+        Piece[] p = sut.run(dPiece);
 
-        assertEquals('D', p.getLetter());
+        assertEquals('D', p[0].getLetter());
     }
 
     @Test
@@ -22,9 +22,9 @@ public class TestPieceGenerator {
         String dPiece = "C 2 3";
         PieceGenerator sut = new PieceGenerator();
 
-        Piece p = sut.generatePiece(dPiece);
+        Piece[] p = sut.run(dPiece);
 
-        assertEquals('C', p.getLetter());
+        assertEquals('C', p[0].getLetter());
     }
 
     @Test
@@ -32,9 +32,9 @@ public class TestPieceGenerator {
         String dPiece = "D 4 2";
         PieceGenerator sut = new PieceGenerator();
 
-        Piece p = sut.generatePiece(dPiece);
+        Piece[] p = sut.run(dPiece);
 
-        assertEquals(2, p.getValue());
+        assertEquals(2, p[0].getValue());
     }
 
     @Test
@@ -42,8 +42,20 @@ public class TestPieceGenerator {
         String dPiece = "E 12 1";
         PieceGenerator sut = new PieceGenerator();
 
-        Piece p = sut.generatePiece(dPiece);
+        Piece[] p = sut.run(dPiece);
 
-        assertEquals(1, p.getValue());
+        assertEquals(1, p[0].getValue());
+    }
+
+    @Test
+    public void parseMultiplePieces(){
+        String pieces = "E 12 1\nD 4 2";
+        PieceGenerator sut = new PieceGenerator();
+
+        Piece[] p = sut.run(pieces);
+
+        assertEquals(2, p.length);
+        assertEquals('E', p[0].getLetter());
+        assertEquals('D', p[1].getLetter());
     }
 }
