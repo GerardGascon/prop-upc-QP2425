b9bba522977ea437e658223231235c14fde17083
Biel <biel.perez@estudiantat.upc.edu>
2025-03-18 17:10:30 +0100

    Turn | Falta la part de PLACEPIECE & UPDATE SCORE |

--- FONTS/main/edu/upc/prop/scrabble/domain/Turn.java
+++ FONTS/main/edu/upc/prop/scrabble/domain/Turn.java
@@ -1,8 +1,8 @@
 package edu.upc.prop.scrabble.domain;
 
-import edu.upc.prop.scrabble.data.Bag;
 import edu.upc.prop.scrabble.data.Piece;
 import edu.upc.prop.scrabble.data.Player;
+import edu.upc.prop.scrabble.utils.Direction;
 
 import java.util.List;
 
@@ -31,10 +31,17 @@ public class Turn {
             currentPlayer.getHand().add(actualPiece);
         }
 
-        //El jugador si pot posa peça
+        //El jugador posa peça
         // DESPRES : if (existeix alguna paraula que puguis posar)
 
+        //  Geri Working on it
+        // DecideWord ----> Piece[] Word, int startX, int startY, Direction direccio
+        Piece[] Word ={new Piece("a",8)};
+        int StartX = 0; int StartY = 0;
+        Direction direction = Direction.Vertical;
 
+        wordPlacer.run(Word,StartX,StartY,direction);
 
+        // CurrentPlayer.score(Update(Word));
     }
 }
