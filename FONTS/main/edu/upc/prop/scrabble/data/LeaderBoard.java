96a3fe9e30e390e7f8a8eb263432bd638576a18b
Felipe Martinez Lassalle <felipe.martinez.lassalle@estudiantat.upc.edu>
2025-03-24 22:00:37 +0100

    He puesto dos maps diferentes en la leaderboard para asi poder clasificarlos por puntuacion o por nombre por si hay que acceder a un jugador especifico no tener que iterar en el map (se que sera un map de pocos players pero asi queda mas clean :3) tendre que ir con ojo de editarlos a la vez siempre. el map basico lo he actualizado pq no habia caido en el caso de que mas de un jugador pudiera tener la misma puntuacion, si quereis q en lugar de una lista sea por orden alfabetico decidmelo. si tengo que cambiar algo a domain tb decidmelo pq aun no tengo muy claro donde debe ir cada cosa

--- FONTS/main/edu/upc/prop/scrabble/data/LeaderBoard.java
+++ FONTS/main/edu/upc/prop/scrabble/data/LeaderBoard.java
@@ -2,26 +2,72 @@ package edu.upc.prop.scrabble.data;
 
 import java.util.Map;
 import java.util.TreeMap;
+import java.util.Iterator;
 import java.util.List;
+import java.util.ArrayList;
 
 public class LeaderBoard {
-    private Map<Integer, String> leaderBoard;
+    private Map<Integer, List<String>> leaderBoard;
+    private Map<String, Integer> playerScores;
 
     public LeaderBoard() { this.leaderBoard = new TreeMap<>(); }
     public LeaderBoard(Score score) {
         this.leaderBoard = new TreeMap<>();
-        leaderBoard.put(score.getScoreValue(), score.getPlayerName());
+        List<String> aux = new ArrayList<>();
+        aux.add(score.getPlayerName());
+        leaderBoard.put(score.getScoreValue(), aux);
+        playerScores.put(score.getPlayerName(), score.getScoreValue());
     }
     public LeaderBoard(List<Score> scoreList) {
         this.leaderBoard = new TreeMap<>();
-        for (Score score : scoreList) { leaderBoard.put(score.getScoreValue(), score.getPlayerName());}
+        for (Score score : scoreList) {
+            if(leaderBoard.containsKey(score.getScoreValue())) {
+                leaderBoard.get(score.getScoreValue()).add(score.getPlayerName());
+            } else {
+                List<String> aux = new ArrayList<>();
+                aux.add(score.getPlayerName());
+                leaderBoard.put(score.getScoreValue(), aux);
+            }
+            playerScores.put(score.getPlayerName(), score.getScoreValue());
+        }
     }
 
-    public void addPlayer(Score score) { leaderBoard.put(score.getScoreValue(), score.getPlayerName()); }
+    public void addPlayer(Score score) {
+        List<String> aux = new ArrayList<>();
+        aux.add(score.getPlayerName());
+        leaderBoard.put(score.getScoreValue(), aux);
+        playerScores.put(score.getPlayerName(), score.getScoreValue());
+    }
 
     public void addPlayers(List<Score> scoreList) {
-        for (Score score : scoreList) { leaderBoard.put(score.getScoreValue(), score.getPlayerName()); }
+        for (Score score : scoreList) {
+            if(leaderBoard.containsKey(score.getScoreValue())) {
+                leaderBoard.get(score.getScoreValue()).add(score.getPlayerName());
+            } else {
+                List<String> aux = new ArrayList<>();
+                aux.add(score.getPlayerName());
+                leaderBoard.put(score.getScoreValue(), aux);
+            }
+            playerScores.put(score.getPlayerName(), score.getScoreValue());
+        }
     }
 
-    public Map<Integer, String> getLeaderBoard() { return leaderBoard; }
+    public Map<Integer, List<String>> getLeaderBoard() { return leaderBoard; }
+
+    public int getPlayerScore(String playerName) { return  playerScores.get(playerName); }
+
+    public List<String> getRank(int rank) {
+
+        Iterator<Map.Entry<Integer, List<String>>> iterator = leaderBoard.entrySet().iterator();
+        int i = 0;
+        while (iterator.hasNext()) {
+            Map.Entry<Integer, List<String>> entry = iterator.next();
+            if (i == rank - 1) {
+                return entry.getValue();
+            }
+            i++;
+        }
+        return new ArrayList<>();
+
+    }
 }
