1f29d959c747f5819f6ba326a0de80afdc627db9
Felipe Martinez Lassalle <felipe.martinez.lassalle@estudiantat.upc.edu>
2025-03-25 19:26:08 +0100

    feat: added isWinner boolean variable to Score

--- FONTS/main/edu/upc/prop/scrabble/data/Score.java
+++ FONTS/main/edu/upc/prop/scrabble/data/Score.java
@@ -2,10 +2,12 @@ package edu.upc.prop.scrabble.data;
 
 public class Score {
     private int scoreValue;
+    private boolean isWinner;
     final private String playerName;
 
-    public Score(int score, String name) {
+    public Score(int score, boolean winner, String name) {
         this.scoreValue = score;
+        this.isWinner = winner;
         this.playerName = name;
     }
 
