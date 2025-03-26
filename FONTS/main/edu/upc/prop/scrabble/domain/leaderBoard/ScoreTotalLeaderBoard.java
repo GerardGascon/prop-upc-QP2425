05f6c822821f897dd1fe59c606c21b93c89a6703
Felipe Martinez Lassalle <felipe.martinez.lassalle@estudiantat.upc.edu>
2025-03-26 13:38:01 +0100

    feat: ScoreTotalLeaderBoard with return function

---
+++ FONTS/main/edu/upc/prop/scrabble/domain/leaderBoard/ScoreTotalLeaderBoard.java
@@ -0,0 +1,28 @@
+package edu.upc.prop.scrabble.domain.leaderBoard;
+
+import edu.upc.prop.scrabble.data.Score;
+import edu.upc.prop.scrabble.domain.score.ScoreTotal;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.stream.Collectors;
+import java.util.Map;
+import java.util.HashMap;
+
+public class ScoreTotalLeaderBoard {
+
+    public ArrayList<ScoreTotal> getScoreTotalLeaderBoard(Score[] scores) {
+        // Group by player name and total score
+        Map<String, Integer> scoreMap = new HashMap<>();
+        for(Score score : scores) scoreMap.put(score.getPlayerName(), scoreMap.getOrDefault(score.getPlayerName(), 0) + score.getScoreValue());
+
+        // Convert map into a sorted ArrayList<ScoreTotal>
+        ArrayList<ScoreTotal> sortedScoreTotal = scoreMap.entrySet().stream()
+                .map(entry -> new ScoreTotal(entry.getKey(), entry.getValue()))
+                .sorted(Comparator.comparingInt(ScoreTotal::getScoreTotal).reversed())
+                .collect(Collectors.toCollection(ArrayList::new));
+
+        return sortedScoreTotal;
+    }
+
+}
