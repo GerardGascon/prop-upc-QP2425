effb921c1fb87c69f10893bc5fe2077014eec1e3
Felipe Martinez Lassalle <felipe.martinez.lassalle@estudiantat.upc.edu>
2025-03-26 14:04:38 +0100

    refactor: removed need of getScoreTotal function

--- FONTS/main/edu/upc/prop/scrabble/domain/leaderBoard/ScoreTotalLeaderBoard.java
+++ FONTS/main/edu/upc/prop/scrabble/domain/leaderBoard/ScoreTotalLeaderBoard.java
@@ -3,25 +3,21 @@ package edu.upc.prop.scrabble.domain.leaderBoard;
 import edu.upc.prop.scrabble.data.Score;
 import edu.upc.prop.scrabble.domain.score.ScoreTotal;
 
-import java.util.ArrayList;
 import java.util.Comparator;
-import java.util.stream.Collectors;
 import java.util.Map;
 import java.util.HashMap;
 
 public class ScoreTotalLeaderBoard {
 
-    public ArrayList<ScoreTotal> getScoreTotalLeaderBoard(Score[] scores) {
+    public ScoreTotal[] run(Score[] scores) {
         // Group by player name and total score
         Map<String, Integer> scoreMap = new HashMap<>();
         for(Score score : scores) scoreMap.put(score.getPlayerName(), scoreMap.getOrDefault(score.getPlayerName(), 0) + score.getScoreValue());
 
         // Convert map into a sorted ArrayList<ScoreTotal>
         return scoreMap.entrySet().stream()
-                .map(entry -> new ScoreTotal(entry.getKey(), entry.getValue()))
-                .sorted(Comparator.comparingInt(ScoreTotal::getScoreTotal).reversed())
-                .collect(Collectors.toCollection(ArrayList::new));
-
+                .sorted((entry1, entry2) -> Integer.compare(entry2.getValue(), entry1.getValue())) // Sort directly using map values
+                .map(entry -> new ScoreTotal(entry.getKey(), entry.getValue())) // Create ScoreTotal objects after sorting
+                .toArray(ScoreTotal[]::new);
     }
-
 }
