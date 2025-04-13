1f344ef7656332fcaaf7c080b6c50c1db9ba45f3
Gerard Gascón <52170489+GerardGascon@users.noreply.github.com>
2025-04-13 08:39:30 +0200

    refactor: better line reader

--- FONTS/main/edu/upc/prop/scrabble/presenter/terminal/utils/Reader.java
+++ FONTS/main/edu/upc/prop/scrabble/presenter/terminal/utils/Reader.java
@@ -1,24 +1,24 @@
 package edu.upc.prop.scrabble.presenter.terminal.utils;
 
-import edu.upc.prop.scrabble.presenter.scenes.SceneManager;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.Scanner;
+import java.util.concurrent.*;
 
 public class Reader {
     private final Scanner scanner = new Scanner(System.in);
+    private final BlockingQueue<String> inputQueue = new LinkedBlockingQueue<>();
+
+    public Reader() {
+        Thread inputThread = new Thread(() -> {
+            while (true) {
+                String line = scanner.nextLine();
+                inputQueue.offer(line);
+            }
+        });
+        inputThread.setDaemon(true);
+        inputThread.start();
+    }
 
     public String readLine() {
-        try {
-            if (System.in.available() > 0) {
-                return scanner.nextLine();
-            }
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-
-        return null;
+        return inputQueue.poll();
     }
 }
