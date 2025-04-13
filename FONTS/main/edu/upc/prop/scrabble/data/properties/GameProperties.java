95ff21878c696dd2301bdcc458f3499548c80f88
Biel Perez <bielperezs@gmail.com>
2025-04-13 19:55:40 +0200

    En game properties ya tiene toda la info de la configuracion de la partida, geri puede usar las funciones que no se usan enlagnuge setter para hacer la carga de palabras y letras :D

--- FONTS/main/edu/upc/prop/scrabble/data/properties/GameProperties.java
+++ FONTS/main/edu/upc/prop/scrabble/data/properties/GameProperties.java
@@ -2,6 +2,7 @@ package edu.upc.prop.scrabble.data.properties;
 
 import edu.upc.prop.scrabble.data.board.BoardType;
 
+//Conte la informacio de la configuracio de la partida :D
 public record GameProperties(Language language, BoardType boardType, int players, int cpus) {
 
 }
