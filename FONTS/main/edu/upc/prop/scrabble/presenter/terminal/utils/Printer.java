98576b14b144904d4c485006db6853bf89cb880b
Gerard Gascón <52170489+GerardGascon@users.noreply.github.com>
2025-04-13 18:57:43 +0200

    chore: remove Printer

--- FONTS/main/edu/upc/prop/scrabble/presenter/terminal/utils/Printer.java
+++
@@ -1,8 +0,0 @@
-package edu.upc.prop.scrabble.presenter.terminal.utils;
-
-public class Printer {
-    @Override
-    public void display(String message) {
-        System.out.println(message);
-    }
-}
