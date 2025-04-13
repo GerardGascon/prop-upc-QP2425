f8ec3d425b28131b35c7ff766e936b0e935a171d
gini <gina.escofet@estudiantat.upc.edu>
2025-04-13 18:32:40 +0200

    feat: Printer

--- FONTS/main/edu/upc/prop/scrabble/presenter/terminal/draw/IDrawReader.java
+++
@@ -1,7 +0,0 @@
-package edu.upc.prop.scrabble.presenter.terminal.draw;
-
-import edu.upc.prop.scrabble.data.pieces.Piece;
-
-public interface IDrawReader {
-    Piece[] run(int n);
-}
---
+++ FONTS/main/edu/upc/prop/scrabble/presenter/terminal/utils/Printer.java
@@ -0,0 +1,8 @@
+package edu.upc.prop.scrabble.presenter.terminal.utils;
+
+public class Printer {
+    @Override
+    public void display(String message) {
+        System.out.println(message);
+    }
+}
