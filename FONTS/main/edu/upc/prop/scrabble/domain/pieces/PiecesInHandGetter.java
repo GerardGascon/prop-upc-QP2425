94734ad12f6a87db6eaeae7263fe2e538611d48f
gini <gina.escofet@estudiantat.upc.edu>
2025-04-13 10:18:04 +0200

    fix : fieces in hand getter and test ok

---
+++ FONTS/main/edu/upc/prop/scrabble/domain/pieces/PiecesInHandGetter.java
@@ -0,0 +1,39 @@
+package edu.upc.prop.scrabble.domain.pieces;
+
+import edu.upc.prop.scrabble.data.Player;
+import edu.upc.prop.scrabble.data.pieces.Bag;
+import edu.upc.prop.scrabble.data.pieces.Piece;
+
+import java.util.Vector;
+
+public class PiecesInHandGetter {
+    private final Player player;
+    private final Bag bag;
+
+    public PiecesInHandGetter( Bag b, Player p) {
+        player = p;
+        bag = b;
+    }
+
+    public Piece[] run(Piece[] pieces) {
+        try {
+            Vector<Piece> hand = player.getHand();
+            int count = 0;
+            for (Piece requiredPieces : pieces) {
+                for (Piece handPieces : hand) {
+                    if (handPieces.equals(requiredPieces)) {
+                        ++count;
+                    }
+                }
+            }
+            if (count == pieces.length) { // tinc totes les peces a la mà per fer la paraula
+                System.out.println("The player has the necessary pieces in hand to play the word.");
+                PieceDrawer pc = new PieceDrawer(bag, player);
+                Piece[] newPieces = pc.run(pieces);
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        return pieces;
+    }
+}
--- FONTS/test/scrabble/pieces/TestPiecesInHandGetter.java
+++ FONTS/test/scrabble/pieces/TestPiecesInHandGetter.java
@@ -1,4 +1,31 @@
 package scrabble.pieces;
 
+import edu.upc.prop.scrabble.data.Player;
+import edu.upc.prop.scrabble.data.pieces.Bag;
+import edu.upc.prop.scrabble.data.pieces.Piece;
+import edu.upc.prop.scrabble.domain.pieces.PieceDrawer;
+import edu.upc.prop.scrabble.domain.pieces.PiecesInHandGetter;
+import org.junit.Test;
+import java.util.*;
+
+import static org.junit.Assert.assertEquals;
+
 public class TestPiecesInHandGetter {
+    @Test
+    public void get1Piece() {
+        Bag bag = new Bag();
+        Piece p1 = new Piece("a", 1);
+        bag.addPiece(p1);
+
+        Player player = new Player("nom", false);
+        for (int i = 0; i < 7; i++) {
+            player.AddPiece(new Piece("b", 1));
+        }
+
+        Piece[] word = new Piece[]{new Piece("b", 1)};
+        PiecesInHandGetter sut = new PiecesInHandGetter(bag, player);
+        Piece[] newPieces = sut.run(word);
+
+        assertEquals(p1, newPieces[0]);
+    }
 }
