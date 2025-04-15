32fab3fc91d1c02675cc218ce66dd9c46fd9f144
Gerard Gascón <ggasconmoline@gmail.com>
2025-04-15 14:05:17 +0200

    feat: better player instantiation

--- FONTS/main/edu/upc/prop/scrabble/presenter/terminal/scenes/GameScene.java
+++ FONTS/main/edu/upc/prop/scrabble/presenter/terminal/scenes/GameScene.java
@@ -6,24 +6,39 @@ import edu.upc.prop.scrabble.domain.board.PointCalculator;
 import edu.upc.prop.scrabble.domain.board.WordGetter;
 import edu.upc.prop.scrabble.domain.board.WordPlacer;
 import edu.upc.prop.scrabble.domain.pieces.PiecesConverter;
+import edu.upc.prop.scrabble.domain.turns.IGamePlayer;
+import edu.upc.prop.scrabble.domain.turns.Turn;
 import edu.upc.prop.scrabble.presenter.scenes.Scene;
 import edu.upc.prop.scrabble.presenter.terminal.BoardView;
 import edu.upc.prop.scrabble.presenter.terminal.factories.PlayerFactory;
 import edu.upc.prop.scrabble.presenter.terminal.players.PlayerObject;
 import edu.upc.prop.scrabble.presenter.terminal.movements.MovementMaker;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class GameScene extends Scene {
     public GameScene(GameProperties properties) {
-        PlayerObject player = instantiate(PlayerObject.class);
-        BoardView boardView = instantiate(BoardView.class);
-
         Board board = getBoard(properties);
         WordGetter wordGetter = new WordGetter(board);
         PointCalculator pointCalculator = new PointCalculator(board, wordGetter);
         PiecesConverter piecesConverter = new PiecesConverter();
 
-        PlayerFactory.createPlayer(player, "Potato", false, piecesConverter, pointCalculator, board, boardView);
-        player.startTurn();
+        IGamePlayer[] players = instantiatePlayers(properties, piecesConverter, pointCalculator, board);
+        Turn turnManager = new Turn(players);
+//        turnManager.run();
+    }
+
+    private PlayerObject[] instantiatePlayers(GameProperties properties, PiecesConverter piecesConverter,
+                                    PointCalculator pointCalculator, Board board) {
+        List<PlayerObject> playerObjects = new ArrayList<>();
+        for (int i = 0; i < properties.players() + properties.cpus(); i++) {
+            PlayerObject player = instantiate(PlayerObject.class);
+            BoardView boardView = instantiate(BoardView.class);
+            PlayerFactory.createPlayer(player, "Potato", false, piecesConverter, pointCalculator, board, boardView);
+            playerObjects.add(player);
+        }
+        return playerObjects.toArray(PlayerObject[]::new);
     }
 
     private Board getBoard(GameProperties properties) {
