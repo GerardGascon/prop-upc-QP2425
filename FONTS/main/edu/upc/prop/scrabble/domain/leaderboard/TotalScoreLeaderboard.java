18f3e8288b8d303911021de299d033e8cf9543e5
Felipe Martinez Lassalle <193937904+Shonatza@users.noreply.github.com>
2025-04-15 15:08:11 +0200

    refactor: name update ScoreTotalLeaderBoard -> TotalScoreLeaderboard

--- FONTS/main/edu/upc/prop/scrabble/domain/leaderboard/TotalScoreLeaderboard.java
+++ FONTS/main/edu/upc/prop/scrabble/domain/leaderboard/TotalScoreLeaderboard.java
@@ -5,17 +5,17 @@ import edu.upc.prop.scrabble.data.leaderboard.Score;
 import java.util.Map;
 import java.util.HashMap;
 
-public class ScoreTotalLeaderboard {
+public class TotalScoreLeaderboard {
 
-    public ScoreTotal[] run(Score[] scores) {
+    public TotalScore[] run(Score[] scores) {
         // Group by player name and total score
         Map<String, Integer> scoreMap = new HashMap<>();
         for(Score score : scores) scoreMap.put(score.getPlayerName(), scoreMap.getOrDefault(score.getPlayerName(), 0) + score.getScoreValue());
 
-        // Convert map into a sorted ScoreTotal[]
+        // Convert map into a sorted TotalScore[]
         return scoreMap.entrySet().stream()
                 .sorted((entry1, entry2) -> Integer.compare(entry2.getValue(), entry1.getValue())) // Sort directly using map values
-                .map(entry -> new ScoreTotal(entry.getKey(), entry.getValue())) // Create ScoreTotal objects after sorting
-                .toArray(ScoreTotal[]::new);
+                .map(entry -> new TotalScore(entry.getKey(), entry.getValue())) // Create ScoreTotal objects after sorting
+                .toArray(TotalScore[]::new);
     }
 }
