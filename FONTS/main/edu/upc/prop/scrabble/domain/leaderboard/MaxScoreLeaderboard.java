391c8c83e6984d40cc1fc6e5e708e05bd59a285e
Felipe Martinez Lassalle <193937904+Shonatza@users.noreply.github.com>
2025-04-15 15:33:02 +0200

    feat & comment: MaxScoreLeaderboard controller and a comment correction

--- FONTS/main/edu/upc/prop/scrabble/domain/leaderboard/MaxScoreLeaderboard.java
+++ FONTS/main/edu/upc/prop/scrabble/domain/leaderboard/MaxScoreLeaderboard.java
@@ -5,17 +5,18 @@ import edu.upc.prop.scrabble.data.leaderboard.Score;
 import java.util.HashMap;
 import java.util.Map;
 
-public class TotalScoreLeaderboard {
+public class MaxScoreLeaderboard   {
 
     public ScorePair[] run(Score[] scores) {
-        // Group by player name and total score
+        // Group by player name and maxScore
         Map<String, Integer> scoreMap = new HashMap<>();
-        for(Score score : scores) scoreMap.put(score.getPlayerName(), scoreMap.getOrDefault(score.getPlayerName(), 0) + score.getScoreValue());
+        for (Score score : scores) scoreMap.compute(score.getPlayerName(), (k, v) -> v == null || score.getScoreValue() > v ? score.getScoreValue() : v);
 
         // Convert map into a sorted ScorePair[]
         return scoreMap.entrySet().stream()
                 .sorted((entry1, entry2) -> Integer.compare(entry2.getValue(), entry1.getValue())) // Sort directly using map values
                 .map(entry -> new ScorePair(entry.getKey(), entry.getValue())) // Create ScorePair objects after sorting
                 .toArray(ScorePair[]::new);
+
     }
 }
