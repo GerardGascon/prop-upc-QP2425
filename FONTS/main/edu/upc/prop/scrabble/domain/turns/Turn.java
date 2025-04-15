b141c1fc56e45a00ad27e36e772bf5ef36d2baa6
Biel Perez <bielperezs@gmail.com>
2025-04-15 17:08:58 +0200

    Estamos en ello

--- FONTS/main/edu/upc/prop/scrabble/domain/turns/Turn.java
+++ FONTS/main/edu/upc/prop/scrabble/domain/turns/Turn.java
@@ -1,49 +1,56 @@
 package edu.upc.prop.scrabble.domain.turns;
 
+
+import static edu.upc.prop.scrabble.domain.turns.TurnResult.Skip;
+
 public class Turn {
+    private final Endgame endgame;
     private final IGamePlayer[] players;
     private int turnNumber;
+    private int skipCounter;
 
-    //Constructora
-    public Turn(IGamePlayer[] players) {
+    public Turn(Endgame endgame, IGamePlayer[] players) {
+        this.endgame = endgame;
         this.players = players;
         this.turnNumber = 0;
+        this.skipCounter = 0;
     }
 
-    public void run(){
-
+    public void run() {
         int start = 0, end = 0;
-        boolean endgame = false;
-        int modTurn = turnNumber%4;
-        while (!endgame) {
-            modTurn = turnNumber%4;
-            switch (modTurn) {
-                case 0 -> {
-                    start = 0;
-                    end = 3;
-                }
-                case 1 -> {
-                    start = 1;
-                    end = 2;
-                }
-                case 2 -> {
-                    start = 2;
-                    end = 1;
-                }
-                case 3 -> {
-                    start = 3;
-                    end = 2;
-                }
-            }
+        int modTurn = turnNumber % players.length;
 
-            players[end].endTurn();
-            players[start].startTurn();
-            turnNumber = turnNumber + 1;
-            endgame = true;
-            //TODO: Winchecker to chekc si s'ha acbaat la partida (en una classe aparte)
-            // Winchdecker una queue de mida 3 (ultimes 3 jugades) per a cadfa jugador que diu si
-            // jugada valida o invalida
+        switch (modTurn) {
+            case 0 -> {
+                start = 0;
+                end = 3;
+            }
+            case 1 -> {
+                start = 1;
+                end = 2;
+            }
+            case 2 -> {
+                start = 2;
+                end = 1;
+            }
+            case 3 -> {
+                start = 3;
+                end = 2;
+            }
         }
+
+        //enum
+        if (players[end].endTurn() == Skip)
+            skipCounter++;
+
+        players[start].startTurn();
+        turnNumber = turnNumber + 1;
+
+
+        boolean gameended = endgame.run(skipCounter);
+        //TODO: ENDGAME! to check si s'ha acbaat la partida (en una classe aparte)
+        // ENDGAME una queue de mida 3 (ultimes 3 jugades) per a cadfa jugador que diu si
+        // jugada valida o invalida
 
     }
 }
