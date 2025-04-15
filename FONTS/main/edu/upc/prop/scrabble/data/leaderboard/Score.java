ccdfc2ffd81c805914c14b1e1c990edee3ee399b
Felipe Martinez Lassalle <193937904+Shonatza@users.noreply.github.com>
2025-04-15 17:50:54 +0200

    doc: data classes Score & Leaderboard

--- FONTS/main/edu/upc/prop/scrabble/data/leaderboard/Score.java
+++ FONTS/main/edu/upc/prop/scrabble/data/leaderboard/Score.java
@@ -1,5 +1,12 @@
 package edu.upc.prop.scrabble.data.leaderboard;
 
+/**
+ * Record of relevant values of a game
+ * @param scoreValue
+ * @param isWinner
+ * @param playerName
+ * @author Felipe Martínez Lassalle
+ */
 public record Score(int scoreValue, boolean isWinner, String playerName) {
 
 }
