8a0e08d67ca70e125f57c093b57581b4f5cb2948
Felipe Martinez Lassalle <193937904+Shonatza@users.noreply.github.com>
2025-04-15 17:53:27 +0200

    doc: Score

--- FONTS/main/edu/upc/prop/scrabble/data/leaderboard/Score.java
+++ FONTS/main/edu/upc/prop/scrabble/data/leaderboard/Score.java
@@ -2,9 +2,9 @@ package edu.upc.prop.scrabble.data.leaderboard;
 
 /**
  * Record of relevant values of a game
- * @param scoreValue
- * @param isWinner
- * @param playerName
+ * @param scoreValue Total score achieved by the player in the game
+ * @param isWinner True if the player won the game
+ * @param playerName Name used by the player
  * @author Felipe Martínez Lassalle
  */
 public record Score(int scoreValue, boolean isWinner, String playerName) {
