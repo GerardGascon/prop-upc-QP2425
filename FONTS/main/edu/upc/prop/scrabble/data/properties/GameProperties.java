651f3534a3f150a00b026e311643c34f3e9d8ea4
Biel Perez <bielperezs@gmail.com>
2025-04-16 19:56:25 +0200

    Update de que GameCreator li arrivi el nom de cpus y nom de players en comptes del numero de cada

--- FONTS/main/edu/upc/prop/scrabble/data/properties/GameProperties.java
+++ FONTS/main/edu/upc/prop/scrabble/data/properties/GameProperties.java
@@ -2,7 +2,14 @@ package edu.upc.prop.scrabble.data.properties;
 
 import edu.upc.prop.scrabble.data.board.BoardType;
 
-//Conte la informacio de la configuracio de la partida :D
-public record GameProperties(Language language, BoardType boardType, int players, int cpus) {
+import java.util.List;
 
+/**
+ * Conte la informacio de la partida
+ * @param language
+ * @param boardType
+ * @param players
+ * @param Cpus
+ */
+public record GameProperties(Language language, BoardType boardType, List<String> players, List<String> Cpus) {
 }
