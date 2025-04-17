2d6f92b7cbc4fa61bf4481a6d1f572fd97f4a29e
Alusat <alusat22@gmail.com>
2025-04-17 15:24:19 +0200

    specialPieces commentado

--- FONTS/main/edu/upc/prop/scrabble/data/crosschecks/CatalanCrossChecks.java
+++ FONTS/main/edu/upc/prop/scrabble/data/crosschecks/CatalanCrossChecks.java
@@ -6,13 +6,13 @@ import edu.upc.prop.scrabble.data.dawg.DAWG;
 public class CatalanCrossChecks extends CrossChecks {
     //CAT: letras normales + pos 27 para la ç, pos 28 para L.L, pos 29 para NY
     private final String[] letters;
-    private final String[] specialPieces;
+    //private final String[] specialPieces;
 
     public CatalanCrossChecks(Board board, DAWG dawg) {
         super(board);
         letters = new String[]{"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M",
                 "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "Ç", "L·L", "NY"};
-        specialPieces = new String[]{"Ç", "L·L", "NY"};
+        //specialPieces = new String[]{"Ç", "L·L", "NY"};
     }
 
     @Override
--- FONTS/main/edu/upc/prop/scrabble/data/crosschecks/SpanishCrossChecks.java
+++ FONTS/main/edu/upc/prop/scrabble/data/crosschecks/SpanishCrossChecks.java
@@ -6,13 +6,13 @@ import edu.upc.prop.scrabble.data.dawg.DAWG;
 public class SpanishCrossChecks extends CrossChecks {
     //ESP: letras normales + pos 27 para la ñ, pos 28 para la RR, 29 LL, 30 CH
     private final String[] letters;
-    private final String[] specialPieces;
+    //private final String[] specialPieces;
 
     public SpanishCrossChecks(Board board, DAWG dawg) {
         super(board);
         letters = new String[]{"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M",
                 "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "Ñ", "RR", "LL", "CH"};
-        specialPieces = new String[]{"Ñ", "RR", "LL", "CH"};
+        //specialPieces = new String[]{"Ñ", "RR", "LL", "CH"};
     }
 
     @Override
