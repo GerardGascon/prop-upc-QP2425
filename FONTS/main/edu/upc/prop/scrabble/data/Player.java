199469ff06bf320a8a2a25e453714fb59d76f09c
Alusat <alusat22@gmail.com>
2025-04-17 12:40:40 +0200

    mod hasPiece

--- FONTS/main/edu/upc/prop/scrabble/data/Player.java
+++ FONTS/main/edu/upc/prop/scrabble/data/Player.java
@@ -90,13 +90,13 @@ public class Player {
 
         throw new PlayerDoesNotHavePieceException("Player " + name + " does not have the piece " + piece.letter());
     }
-    public Boolean hasPiece(Piece piece) {
+    public Piece hasPiece(String piece) {
         Piece actual;
         for (Piece value : hand) {
             actual = value;
-            if (actual.isBlank()) return true;//tenemos todas
-            else if (actual.letter().equals(piece.letter())) return true;
+            if (actual.isBlank()) return actual;//tenemos todas
+            else if (actual.letter().equals(piece)) return actual;
         }
-        return false;
+        return null;
     }
 }
