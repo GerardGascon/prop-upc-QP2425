c031e3f5854cbc1bcb7a6bff9d01077b3b195322
Biel Perez <bielperezs@gmail.com>
2025-04-19 23:20:37 +0200

    ARREGLAT ENDTURN

--- FONTS/main/edu/upc/prop/scrabble/domain/turns/Turn.java
+++ FONTS/main/edu/upc/prop/scrabble/domain/turns/Turn.java
@@ -5,7 +5,6 @@ import static edu.upc.prop.scrabble.domain.turns.TurnResult.Skip;
 
 /**
  * Class used to manage the turn flow in the game
- *
  * @author Biel Pérez
  */
 public class Turn {
@@ -24,20 +23,21 @@ public class Turn {
     /**
      * Determine which player ends its turn and which starts it as well
      * as keeping track of how many turns have passed and if this turn is the one that ends the game
-     *
      * @see Endgame
      * @see IGamePlayer
      */
     public boolean run(TurnResult result) {
-        int startTurn = (turnNumber + 1) % players.length;
+        int endTurn = turnNumber % players.length;
+        int startTurn = (turnNumber+1) % players.length;
 
         if (result == Skip)
             skipCounter++;
         else
             skipCounter = 0;
 
+        players[endTurn].endTurn();
         players[startTurn].startTurn();
-        turnNumber++;
+        turnNumber = turnNumber + 1;
 
         return endgame.run(skipCounter);
     }
