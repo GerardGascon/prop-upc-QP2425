efeac8a668e99fca8dc0cb5153967369eafa15a4
Gerard Gascón <ggasconmoline@gmail.com>
2025-04-20 13:19:10 +0200

    refactor: move gameloop to another class

--- FONTS/main/edu/upc/prop/scrabble/Main.java
+++ FONTS/main/edu/upc/prop/scrabble/Main.java
@@ -1,5 +1,6 @@
 package edu.upc.prop.scrabble;
 
+import edu.upc.prop.scrabble.presenter.GameLoop;
 import edu.upc.prop.scrabble.presenter.scenes.SceneManager;
 import edu.upc.prop.scrabble.presenter.terminal.scenes.GameScene;
 import edu.upc.prop.scrabble.presenter.terminal.scenes.MenuScene;
@@ -8,19 +9,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Welcome to the Scrabble");
 
-        SceneManager.getInstance().load(MenuScene.class);
-
-        long lastTime = System.nanoTime();
-        double delta;
-
-        while(SceneManager.getInstance().isRunning()){
-            long now = System.nanoTime();
-            delta = (now - lastTime) / 1_000_000_000.0;
-            lastTime = now;
-
-            SceneManager.getInstance().process((float)delta);
-//            System.out.println(delta);
-        }
+        GameLoop gameLoop = new GameLoop(MenuScene.class);
+        gameLoop.run();
 
         System.out.println("Bye.");
     }
---
+++ FONTS/main/edu/upc/prop/scrabble/presenter/GameLoop.java
@@ -0,0 +1,32 @@
+package edu.upc.prop.scrabble.presenter;
+
+import edu.upc.prop.scrabble.presenter.scenes.Scene;
+import edu.upc.prop.scrabble.presenter.scenes.SceneManager;
+
+public class GameLoop {
+    private long lastTime;
+
+    public GameLoop(Class<? extends Scene> initialScene) {
+        SceneManager.getInstance().load(initialScene);
+    }
+
+    public void run() {
+        lastTime = System.nanoTime();
+        final double fps = 60.0;
+        final double frameTime = 1e9 / fps;
+
+        while (SceneManager.getInstance().isRunning()) {
+            long now = System.nanoTime();
+            double delta = (now - lastTime) / 1e9;
+            lastTime = now;
+
+            SceneManager.getInstance().process((float)delta);
+
+            try {
+                long sleep = (long) ((frameTime - (System.nanoTime() - now)) / 1e6);
+                if (sleep > 0)
+                    Thread.sleep(sleep);
+            } catch (InterruptedException ignored) {}
+        }
+    }
+}
