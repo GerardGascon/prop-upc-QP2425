db616b55c4fff7d47a8ddf50645ebed3dd035582
Alusat <alusat22@gmail.com>
2025-04-20 11:48:43 +0200

    PlayerDoesNotHavePieceException to ScrabbleException

--- FONTS/main/edu/upc/prop/scrabble/data/exceptions/PlayerDoesNotHavePieceException.java
+++ FONTS/main/edu/upc/prop/scrabble/data/exceptions/PlayerDoesNotHavePieceException.java
@@ -1,6 +1,6 @@
 package edu.upc.prop.scrabble.data.exceptions;
 
-public class PlayerDoesNotHavePieceException extends RuntimeException {
+public class PlayerDoesNotHavePieceException extends ScrabbleException {
     public PlayerDoesNotHavePieceException(String message) {
         super(message);
     }
