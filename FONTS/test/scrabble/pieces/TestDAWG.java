feada953a2c54a4f6bdb0eee08c6e7d47fb5e6da
Alusat <alusat22@gmail.com>
2025-04-22 13:42:42 +0200

    removed previously added unnecessary test

--- FONTS/test/scrabble/pieces/TestDAWG.java
+++ FONTS/test/scrabble/pieces/TestDAWG.java
@@ -40,39 +40,4 @@ public class TestDAWG {
         assertFalse(sut.run("LLAVER"));
     }
 
-    @Test
-    public void addRareWords(){
-        DAWG dawg = new DAWG();
-        WordAdder adder = new WordAdder(dawg);
-        WordValidator sut = new WordValidator(dawg);
-
-        adder.run("CL·LAVE");
-        adder.run("CL·LAVERO");
-
-        assertTrue(sut.run("CL·LAVE"));
-        assertTrue(sut.run("CL·LAVERO"));
-        assertFalse(sut.run("CL·LAVER"));
-
-        adder.run("DCHAVE");
-        adder.run("DCHAVERO");
-
-        assertTrue(sut.run("DCHAVE"));
-        assertTrue(sut.run("DCHAVERO"));
-        assertFalse(sut.run("DCHAVER"));
-
-        adder.run("ÑAVE");
-        adder.run("ÑAVERO");
-
-        assertTrue(sut.run("ÑAVE"));
-        assertTrue(sut.run("ÑAVERO"));
-        assertFalse(sut.run("ÑAVER"));
-
-        adder.run("XÇAVE");
-        adder.run("XÇAVERO");
-
-        assertTrue(sut.run("XÇAVE"));
-        assertTrue(sut.run("XÇAVERO"));
-        assertFalse(sut.run("XÇAVER"));
-    }
-
 }
