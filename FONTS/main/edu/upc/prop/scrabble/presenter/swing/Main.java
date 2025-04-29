4d63d64e2759b087e61aba6d47122729fc41d4ed
Gerard Gascón <ggasconmoline@gmail.com>
2025-04-29 22:17:14 +0200

    feat: swing window creation

--- FONTS/main/edu/upc/prop/scrabble/presenter/swing/Main.java
+++ FONTS/main/edu/upc/prop/scrabble/presenter/swing/Main.java
@@ -1,10 +1,27 @@
 package edu.upc.prop.scrabble.presenter.swing;
 
+import javax.swing.*;
+import java.awt.*;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Welcome to the Scrabble");
 
+        SwingUtilities.invokeLater(() -> {
+            GraphicsEnvironment env = GraphicsEnvironment
+                    .getLocalGraphicsEnvironment();
+            GraphicsDevice device = env.getDefaultScreenDevice();
+            JFrame startWindow = new JFrame("Scrabble");
+            startWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            startWindow.setUndecorated(true);
+            startWindow.setExtendedState(JFrame.MAXIMIZED_BOTH);
+            startWindow.pack();
+            startWindow.setResizable(false);
+            startWindow.validate();
 
+            device.setFullScreenWindow(startWindow);
+            startWindow.setVisible(true);
+        });
 
         System.out.println("Bye.");
     }
