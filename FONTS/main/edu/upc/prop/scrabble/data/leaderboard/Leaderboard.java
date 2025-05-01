19f73b9b1ece2e8a717d8ac04fe22f4aaac964b3
Felipe Martinez Lassalle <193937904+Shonatza@users.noreply.github.com>
2025-05-01 18:22:32 +0200

    feat: made leadearBoard persistable

--- FONTS/main/edu/upc/prop/scrabble/data/leaderboard/Leaderboard.java
+++ FONTS/main/edu/upc/prop/scrabble/data/leaderboard/Leaderboard.java
@@ -1,15 +1,19 @@
 package edu.upc.prop.scrabble.data.leaderboard;
 
+import edu.upc.prop.scrabble.persistence.runtime.data.PersistentArray;
+import edu.upc.prop.scrabble.persistence.runtime.data.PersistentDictionary;
+import edu.upc.prop.scrabble.persistence.runtime.data.PersistentObject;
+import edu.upc.prop.scrabble.persistence.runtime.interfaces.IPersistableObject;
+
 import java.util.ArrayList;
+import java.util.Arrays;
 
 /**
  * Class used to store Score ArrayList
  * @author Felipe Martínez Lassalle
  */
-public class Leaderboard {
-    private final ArrayList<Score> leaderBoard;
-
-    public Leaderboard() { this.leaderBoard = new ArrayList<>(); }
+public class Leaderboard implements IPersistableObject {
+    private ArrayList<Score> leaderBoard = new ArrayList<>();
 
     /**
      * Transforms the ArrayList to a regular array to ease controllers functions
@@ -24,4 +28,17 @@ public class Leaderboard {
      * @see Score
      */
     public void addScore(Score score) { leaderBoard.add(score); }
+
+    @Override
+    public PersistentDictionary encode() {
+        PersistentDictionary data = new PersistentDictionary();
+        data.add(new PersistentObject("leaderboard", leaderBoard.toArray(Score[]::new)));
+        return data;
+    }
+
+    @Override
+    public void decode(PersistentDictionary data) {
+        PersistentObject leaderboard = data.get("leaderboard");
+        leaderBoard.addAll(Arrays.stream(leaderboard.parse(Score[].class)).toList());
+    }
 }
