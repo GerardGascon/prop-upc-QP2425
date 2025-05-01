53201749b26ca327655927d53c79b571fb377732
Gerard Gascón <ggasconmoline@gmail.com>
2025-05-01 20:42:44 +0200

    fix: main window creation for different OS

--- FONTS/main/edu/upc/prop/scrabble/presenter/swing/Main.java
+++ FONTS/main/edu/upc/prop/scrabble/presenter/swing/Main.java
@@ -9,12 +9,34 @@ public class Main {
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
             JFrame window = createMainWindow();
+
             window.add(new GameMock());
             window.setVisible(true);
         });
     }
 
     private static JFrame createMainWindow() {
+        String os = System.getProperty("os.name").toLowerCase();
+        if (os.contains("windows"))
+            return createMainWindow_Windows();
+        else
+            return createMainWindow_Linux();
+    }
+
+    private static JFrame createMainWindow_Windows() {
+        JFrame window = new JFrame();
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        GraphicsDevice gd = GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice();
+        window.setUndecorated(true);
+        window.setResizable(false);
+        window.setSize(gd.getDisplayMode().getWidth(), gd.getDisplayMode().getHeight());
+        window.setExtendedState(JFrame.MAXIMIZED_BOTH);
+        window.setLocationRelativeTo(null);
+        return window;
+    }
+
+    private static JFrame createMainWindow_Linux() {
         JFrame window = new JFrame();
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         window.setUndecorated(true);
