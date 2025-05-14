b0b040856452184789de08c847d280bb763bfd6e
Felipe Martinez Lassalle <193937904+Shonatza@users.noreply.github.com>
2025-05-14 23:32:58 +0200

    feat: created MainMenu file (no work done yet srry ;_;)

---
+++ FONTS/main/edu/upc/prop/scrabble/presenter/swing/screens/menu/MainMenu.java
@@ -0,0 +1,10 @@
+package edu.upc.prop.scrabble.presenter.swing.screens.menu;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class MainMenu extends JPanel {
+
+    public MainMenu() {
+    }
+}
