527c4b8111d0e0e197d306e56d6d2e17686fb71f
Felipe Martinez Lassalle <193937904+Shonatza@users.noreply.github.com>
2025-05-14 23:43:17 +0200

    feat: created RanquingButton (no work done yet srry ;_;)

---
+++ FONTS/main/edu/upc/prop/scrabble/presenter/swing/screens/menu/RanquingButton.java
@@ -0,0 +1,12 @@
+package edu.upc.prop.scrabble.presenter.swing.screens.menu;
+
+import edu.upc.prop.scrabble.presenter.swing.screens.menu.MenuButton;
+
+import javax.swing.*;
+
+public class RanquingButton extends MenuButton {
+
+    public RanquingButton(JPanel parent) {
+        super("Rànquing");
+    }
+}
