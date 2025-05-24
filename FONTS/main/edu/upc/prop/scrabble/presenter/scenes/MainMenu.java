91ad9ff11fb07b390e493833f6ff51547d28b96f
Gerard Gascón <ggasconmoline@gmail.com>
2025-05-24 23:08:37 +0200

    chore: moved main menu to proper folder

--- FONTS/main/edu/upc/prop/scrabble/presenter/scenes/MainMenu.java
+++
@@ -1,5 +0,0 @@
-package edu.upc.prop.scrabble.presenter.scenes;
-
-public class MainMenu {
-
-}
---
+++ FONTS/main/edu/upc/prop/scrabble/presenter/swing/scenes/MainMenu.java
@@ -0,0 +1,5 @@
+package edu.upc.prop.scrabble.presenter.swing.scenes;
+
+public class MainMenu {
+
+}
