a02039f8caa9f62871dba1d03e37c90010ef3fb9
Gerard Gascón <ggasconmoline@gmail.com>
2025-05-24 17:56:33 +0200

    docs: translate action maker

--- FONTS/main/edu/upc/prop/scrabble/domain/actionmaker/IHandView.java
+++ FONTS/main/edu/upc/prop/scrabble/domain/actionmaker/IHandView.java
@@ -2,9 +2,32 @@ package edu.upc.prop.scrabble.domain.actionmaker;
 
 import edu.upc.prop.scrabble.data.pieces.Piece;
 
+/**
+ * Interfície que representa la vista de la mà del jugador en el joc de Scrabble.
+ * Permet consultar la peça actualment seleccionada i mostrar el conjunt de peces disponibles.
+ *
+ * @author Gina Escofet
+ */
 public interface IHandView {
+    /**
+     * Retorna la peça actualment seleccionada com un array de cadenes.
+     * El format de les cadenes depèn de la implementació (per exemple, lletra i marques especials).
+     *
+     * @return Un array de cadenes que representen la peça seleccionada.
+     */
     String[] getSelectedPiece();
+
+    /**
+     * Retorna el valor en punts de la peça actualment seleccionada.
+     *
+     * @return Els punts de la peça seleccionada.
+     */
     int getSelectedPiecePoints();
 
+    /**
+     * Mostra el conjunt de peces proporcionat al jugador.
+     *
+     * @param pieces L'array de peces que s'han de mostrar.
+     */
     void showPieces(Piece[] pieces);
 }
--- FONTS/main/edu/upc/prop/scrabble/domain/actionmaker/SkipActionMaker.java
+++ FONTS/main/edu/upc/prop/scrabble/domain/actionmaker/SkipActionMaker.java
@@ -3,14 +3,32 @@ package edu.upc.prop.scrabble.domain.actionmaker;
 import edu.upc.prop.scrabble.domain.game.GameStepper;
 import edu.upc.prop.scrabble.domain.turns.TurnResult;
 
+/**
+ * Classe que gestiona l'acció de passar el torn en el joc de Scrabble.
+ * Quan s'executa, simplement avança el joc al següent torn sense realitzar cap moviment.
+ *
+ * @author Gerard Gascón
+ */
 public class SkipActionMaker {
+    /**
+     * Component que controla la lògica d'avançar el torn del joc.
+     */
     private final GameStepper stepper;
 
+    /**
+     * Crea una instància de SkipActionMaker amb el controlador de torns.
+     *
+     * @param stepper Component per avançar la lògica del joc.
+     */
     public SkipActionMaker(GameStepper stepper) {
         this.stepper = stepper;
     }
 
+    /**
+     * Executa l'acció de passar el torn, indicant al controlador que s'ha saltat el torn.
+     */
     public void run() {
         stepper.run(TurnResult.Skip);
     }
 }
+
