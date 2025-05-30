bb0d1bc6675bd326cde80efff8946f78fb5825ba
Gerard Gascón <ggasconmoline@gmail.com>
2025-05-30 10:17:12 +0200

    test: failing java tests for dawg

--- FONTS/test/scrabble/pieces/TestDAWG.java
+++ FONTS/test/scrabble/pieces/TestDAWG.java
@@ -5,6 +5,12 @@ import edu.upc.prop.scrabble.domain.dawg.WordAdder;
 import edu.upc.prop.scrabble.domain.dawg.WordValidator;
 import org.junit.Test;
 
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Objects;
+
 import static org.junit.Assert.*;
 
 public class TestDAWG {
@@ -40,4 +46,55 @@ public class TestDAWG {
         assertFalse(sut.run("LLAVER"));
     }
 
+    @Test
+    public void testCatalanDictionary() throws IOException, URISyntaxException {
+        DAWG dawg = new DAWG();
+        WordAdder adder = new WordAdder(dawg);
+        WordValidator sut = new WordValidator(dawg);
+
+        String content = new String(Files.readAllBytes(
+                Paths.get(Objects.requireNonNull(getClass().getClassLoader().getResource("catalan.txt")).toURI())
+        ));
+        content.lines().forEach(adder::run);
+
+        assertFalse("TRAINTS should not exist in catalan dictionary", sut.run("TRAINTS"));
+        assertFalse("DARIENE should not exist in catalan dictionary", sut.run("DARIENE"));
+        assertFalse("URSINAM should not exist in catalan dictionary", sut.run("URSINAM"));
+        assertFalse("ERTALDE should not exist in catalan dictionary", sut.run("ERTALDE"));
+        assertFalse("CREADAS should not exist in catalan dictionary", sut.run("CREADAS"));
+    }
+
+    @Test
+    public void testSpanishDictionary() throws IOException, URISyntaxException {
+        DAWG dawg = new DAWG();
+        WordAdder adder = new WordAdder(dawg);
+        WordValidator sut = new WordValidator(dawg);
+
+        String content = new String(Files.readAllBytes(
+                Paths.get(Objects.requireNonNull(getClass().getClassLoader().getResource("castellano.txt")).toURI())
+        ));
+        content.lines().forEach(adder::run);
+
+        assertFalse("RIFEÑOSE should not exist in spanish dictionary", sut.run("RIFEÑOSE"));
+        assertFalse("ALENGUOS should not exist in spanish dictionary", sut.run("ALENGUOS"));
+        assertFalse("HEZAR should not exist in spanish dictionary", sut.run("HEZAR"));
+        assertFalse("JINGLANDOSE should not exist in spanish dictionary", sut.run("JINGLANDOSE"));
+    }
+
+    @Test
+    public void testEnglishDictionary() throws IOException, URISyntaxException {
+        DAWG dawg = new DAWG();
+        WordAdder adder = new WordAdder(dawg);
+        WordValidator sut = new WordValidator(dawg);
+
+        String content = new String(Files.readAllBytes(
+                Paths.get(Objects.requireNonNull(getClass().getClassLoader().getResource("english.txt")).toURI())
+        ));
+        content.lines().forEach(adder::run);
+
+        assertFalse("PANAXEST should not exist in english dictionary", sut.run("PANAXEST"));
+        assertFalse("TELDENZ should not exist in english dictionary", sut.run("TELDENZ"));
+        assertFalse("TACKEYS should not exist in english dictionary", sut.run("TACKEYS"));
+        assertFalse("CRIBSONS should not exist in english dictionary", sut.run("CRIBSONS"));
+    }
 }
