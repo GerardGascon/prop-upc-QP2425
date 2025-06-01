0c224a1e8b78021d7d42f19dcc6732ad2ab650f0
gini <biel.perez@estudiantat.upc.edu>
2025-06-01 19:46:44 +0200

    Gina!

--- FONTS/main/edu/upc/prop/scrabble/domain/pieces/EnglishPiecesConverter.java
+++ FONTS/main/edu/upc/prop/scrabble/domain/pieces/EnglishPiecesConverter.java
@@ -5,13 +5,25 @@ import edu.upc.prop.scrabble.data.pieces.Piece;
 
 /**
  * Classe que converteix una cadena de lletres en peces en anglès.
+ * Estén {@link PiecesConverter} per oferir la funcionalitat de conversió específica per a l'idioma anglès.
+ * Utilitza el diccionari per defecte o un personalitzat per determinar les puntuacions de les peces.
+ *
  * @author Gina Escofet González
  */
 public class EnglishPiecesConverter extends PiecesConverter {
+    /**
+     * Crea una instància del convertidor de fitxes per a l'idioma anglès amb el diccionari per defecte.
+     * Aquest diccionari hauria de contenir totes les fitxes bàsiques del joc en anglès.
+     */
     public EnglishPiecesConverter() {
         super();
     }
-
+    /**
+     * Crea una instància del convertidor de fitxes per a l'idioma anglès amb un diccionari personalitzat.
+     * Aquest diccionari permet definir fitxes i puntuacions específiques per al joc en anglès.
+     *
+     * @param dictionary Array de fitxes {@link Piece} amb les puntuacions i lletres disponibles.
+     */
     public EnglishPiecesConverter(Piece[] dictionary) {
         super(dictionary);
     }
--- FONTS/main/edu/upc/prop/scrabble/domain/pieces/PieceDrawer.java
+++ FONTS/main/edu/upc/prop/scrabble/domain/pieces/PieceDrawer.java
@@ -43,14 +43,25 @@ public class PieceDrawer {
         for (Piece piece : drawnPieces)
             player.addPiece(piece);
     }
-
+    /**
+     * Intercanvia les peces indicades amb el jugador:
+     * elimina cada peça de la mà del jugador i la retorna a la bossa.
+     *
+     * @param piecesToSwap Array de peces que el jugador vol retornar a la bossa.
+     */
     private void swapPiecesWithPlayer(Piece[] piecesToSwap) {
         for (Piece piece : piecesToSwap) {
             player.removePiece(piece);
             bag.add(piece);
         }
     }
-
+    /**
+     * Extreu de la bossa un nombre de peces aleatòries igual al nombre de peces retornades.
+     * Utilitza el generador aleatori proporcionat per seleccionar les peces.
+     *
+     * @param piecesToSwap Array de peces que el jugador ha retornat a la bossa.
+     * @return Llista de noves peces extretes aleatòriament de la bossa.
+     */
     private List<Piece> collectPiecesFromBag(Piece[] piecesToSwap) {
         List<Piece> drawnPieces = new ArrayList<>();
         for (int i = 0; i < piecesToSwap.length; i++) {
