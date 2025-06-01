434de9e43f75721ad533b59d48e9cb13f6893397
Alusat <alusat22@gmail.com>
2025-06-01 20:55:53 +0200

    Javadoc

--- FONTS/main/edu/upc/prop/scrabble/data/Player.java
+++ FONTS/main/edu/upc/prop/scrabble/data/Player.java
@@ -6,7 +6,14 @@ import edu.upc.prop.scrabble.data.pieces.Piece;
 import java.util.ArrayList;
 
 /**
- * Classe que representa un jugador/a amb els seus atributs
+ * Classe que representa un jugador/a amb els seus atributs.
+ * <p>
+ * Gestiona:
+ * <ul>
+ *   <li>El nom i tipus (humà/CPU) del jugador</li>
+ *   <li>La puntuació acumulada</li>
+ *   <li>Les peces a la mà (màxim 7)</li>
+ * </ul>
  * @author Albert Usero
  */
 public class Player {
@@ -65,25 +72,33 @@ public class Player {
 
     /**
      * Afegeix la quantitat de puntuació de l'input a la puntuació actual del jugador/a
-     * @param scoretoadd Puntuació que volem afegir
+     * @param scoretoadd Puntuació que volem afegir (ha de ser >= 0)
      */
     public void addScore(int scoretoadd) {
         this.score += scoretoadd;
     }
 
+    /**
+     * Retorna un array amb totes les peces que té el jugador a la mà.
+     * @return Array de peces en la mà del jugador
+     * @see Piece
+     */
     public Piece[] getHand() {return hand.toArray(Piece[]::new);}
 
     /**
-     * Afegeix la peça donada a la mà del jugador/a
-     * @param piece Peça que serà afegida
+     * Afegeix la peça donada a la mà del jugador/a.
+     * @param piece Peça que serà afegida (no pot ser null)
      * @see Piece
      */
     public void addPiece(Piece piece) {
         hand.add(piece);
     }
+
     /**
-     * Elimina la peça donada de la mà del jugador/a
+     * Elimina la peça donada de la mà del jugador/a.
      * @param piece Peça que serà eliminada
+     * @return La peça eliminada
+     * @throws PlayerDoesNotHavePieceException si el jugador no té la peça demanada
      * @see Piece
      */
     public Piece removePiece(Piece piece) {
@@ -112,7 +127,7 @@ public class Player {
     /**
      * Consulta si el jugador/a té certa peça a la mà i si és així la retorna.
      * Si no tenim la peça, però sí que tenim comodí, es retornarà el comodí.
-     * @param piece Peça sobre la qual volem obtenir informació
+     * @param piece Peça sobre la qual volem obtenir informació (String que representa la lletra)
      * @return Peça en cas que sí que existeixi, null altrament
      */
     public Piece hasPiece(String piece) {
